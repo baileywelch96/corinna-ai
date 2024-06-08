197a91b8e131e9f18b5729cf28cca13428cc6c9e
Bailey Welch <107787779+baileywelch96@users.noreply.github.com>
2024-06-08 22:15:56 +1000

    formatting

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,24 +1,30 @@
-import { currentUser } from '@clerk/nextjs'
-import Image from 'next/image'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import React from "react";
 
 type Props = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const Layout = async ({ children }: Props) => {
-    const user = await currentUser()
+  const user = await currentUser();
 
-    if (user) redirect('/')
+  if (user) redirect("/");
   return (
-    <div className='h-screen w-full flex justify-center'>
-        <div className='w-[600px] lg:w-full flex flex-col items-start p-6'>
-            <Image src="/images/logo.png" alt='logo' style={{ width: '20%', height: 'auto'}} width={0} height={0} />
-            {children}
-        </div>
+    <div className="h-screen w-full flex justify-center">
+      <div className="w-[600px] lg:w-full flex flex-col items-start p-6">
+        <Image
+          src="/images/logo.png"
+          alt="logo"
+          style={{ width: "20%", height: "auto" }}
+          width={0}
+          height={0}
+        />
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
